3774e07f03e4cd88b1c3da7f87a2cf6d9aa19d50
Andie Fonderson <Andrea.Fonderson@IRIS.CO.UK>
2021-11-24 13:37:47 +0000

    Added test Request API function.

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,5 +16,13 @@ namespace BusBoard.ConsoleApp
             Console.WriteLine(busCode + " Good job.");
             Console.ReadLine();
     }
+
+    static void RequestApi()
+        {
+            var client = new RestClient("https://api.tfl.gov.uk/StopPoint/940GZZLUASL/Arrivals?stopcode=490008660N&mode=bus");
+            var request = new RestRequest("statuses/home_timeline.json", DataFormat.Json);
+
+            var response = client.Get(request);
+        }
   }
 }
